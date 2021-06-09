eedd5cd050ac07244f9fc2ef75524922ae3b83f2
Cody Adcock <codyadcock10@gmail.com>
2021-06-08 21:22:27 -0600

    made it so the webpage runs code after load

--- script.js
+++ script.js
@@ -40,63 +40,68 @@ backgroundLayerForest9.src = 'resources/Forest/PNG/Background layers/Layer_0009_
 const backgroundLayerForest10 = new Image();
 backgroundLayerForest10.src = 'resources/Forest/PNG/Background layers/Layer_0010_1.png'
 
-const slider = document.getElementById('slider');
-slider.value = gameSpeed;
-const showGameSpeed = document.getElementById('showGameSpeed');
-showGameSpeed.innerHTML = gameSpeed;
+window.addEventListener('load', function(){
 
-slider.addEventListener('change', function(e){
-    gameSpeed = e.target.value;
+    const slider = document.getElementById('slider');
+    slider.value = gameSpeed;
+    const showGameSpeed = document.getElementById('showGameSpeed');
     showGameSpeed.innerHTML = gameSpeed;
+    
+    slider.addEventListener('change', function(e){
+        gameSpeed = e.target.value;
+        showGameSpeed.innerHTML = gameSpeed;
+    });
+    
+    class Layer {
+    
+        constructor(image, speedModifier){
+            this.x = 0;
+            this.y = -93;
+            this.width = 928;
+            this.height = 793;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * this.speedModifier;
+        }
+        update(){
+            this.speed = gameSpeed * this.speedModifier;
+            if (this.x <= -this.width){
+                this.x = 0;
+            }
+            this.x = this.x - this.speed;
+        }
+        draw(){
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
+    
+    }
+    
+    const layer0 = new Layer(backgroundLayerForest0, 0.8);
+    const layer1 = new Layer(backgroundLayerForest1, 0.75);
+    const layer2 = new Layer(backgroundLayerForest2, 0.7);
+    const layer3 = new Layer(backgroundLayerForest3, 0.65);
+    const layer4 = new Layer(backgroundLayerForest4, 0.6);
+    const layer5 = new Layer(backgroundLayerForest5, 0.55);
+    const layer6 = new Layer(backgroundLayerForest6, 0.5);
+    const layer7 = new Layer(backgroundLayerForest7, 0.45);
+    const layer8 = new Layer(backgroundLayerForest8, 0.4);
+    const layer9 = new Layer(backgroundLayerForest9, 0.35);
+    const layer10 = new Layer(backgroundLayerForest10, 0);
+    
+    const gameObjects = [layer10, layer9, layer8, layer7, layer6, layer5, layer4, layer3, layer2, layer1, layer0];
+    
+    function animate(){
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        gameObjects.forEach(object => {
+            object.update();
+            object.draw();
+        })
+        requestAnimationFrame(animate);
+    
+    };
+    
+    animate();
+
 });
 
-class Layer {
-
-    constructor(image, speedModifier){
-        this.x = 0;
-        this.y = -93;
-        this.width = 928;
-        this.height = 793;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gameSpeed * this.speedModifier;
-    }
-    update(){
-        this.speed = gameSpeed * this.speedModifier;
-        if (this.x <= -this.width){
-            this.x = 0;
-        }
-        this.x = this.x - this.speed;
-    }
-    draw(){
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-    }
-
-}
-
-const layer0 = new Layer(backgroundLayerForest0, 0.8);
-const layer1 = new Layer(backgroundLayerForest1, 0.75);
-const layer2 = new Layer(backgroundLayerForest2, 0.7);
-const layer3 = new Layer(backgroundLayerForest3, 0.65);
-const layer4 = new Layer(backgroundLayerForest4, 0.6);
-const layer5 = new Layer(backgroundLayerForest5, 0.55);
-const layer6 = new Layer(backgroundLayerForest6, 0.5);
-const layer7 = new Layer(backgroundLayerForest7, 0.45);
-const layer8 = new Layer(backgroundLayerForest8, 0.4);
-const layer9 = new Layer(backgroundLayerForest9, 0.35);
-const layer10 = new Layer(backgroundLayerForest10, 0);
-
-const gameObjects = [layer10, layer9, layer8, layer7, layer6, layer5, layer4, layer3, layer2, layer1, layer0];
-
-function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    gameObjects.forEach(object => {
-        object.update();
-        object.draw();
-    })
-    requestAnimationFrame(animate);
-
-};
-
-animate();
